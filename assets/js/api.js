5f645f59af1cc6fb3c474d0c14336c77ce4491c8
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-08-08 12:44:08 -0300

    caminho do profile.json no api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Gleicianegaldino/Desafio-Developer-Portfolio/main/profile.json';
+    const url = 
+    'https://raw.githubusercontent.com/Gleicianegaldino/Desafio-Developer-Portfolio/main/dados/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
